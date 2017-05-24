1241203d2b4063e220430a963b86c813b00aae61
ssumodhe <ssumodhe@student.42.fr>
2017-05-25 00:30:23 +0200

    merge sort OK | to do: imp to code + -r

--- versions/tests_mergesort.c
+++ versions/tests_mergesort.c
@@ -15,28 +15,109 @@
 typedef struct		s_merge
 {
 	int					i;
+	char *str;
 	struct s_merge 		*next;
 }					t_merge;
 
+int		ft_lstcount(t_merge **merge)
+{
+	t_merge	*tmp;
+	int		nb_list;
 
-void	ft_split_mergesort(t_merge **merge, int size_lst)
+	nb_list = 0;
+	tmp = *merge;
+	while (tmp)
+	{
+		nb_list++;
+		tmp = tmp->next;
+	}
+	return (nb_list);
+}
+
+void	ft_ascii_mergesort(t_merge **tmp, t_merge **left, t_merge **right)
+{
+	if ((ft_strcmp(left->str, right->str)) > 0)
+	{
+		*tmp = *left;
+		*left = (*left)->next;
+	}
+	else
+	{
+		*tmp = *right;
+		*right = (*right)->next;
+	}
+}
+
+void	ft_descendingorder_mergesort(t_merge **tmp, t_merge **left, t_merge **right)
+{
+	if ((*left)->i > (*right)->i)
+	{
+		*tmp = *left;
+		*left = (*left)->next;
+	}
+	else
+	{
+		*tmp = *right;
+		*right = (*right)->next;
+	}
+}
+
+t_merge *ft_glue_mergesort(t_merge *left, t_merge *right)
+{
+	t_merge		*tmp;
+	t_merge		*head;
+	t_merge		*tail;
+
+	head = NULL;
+	while (left || right)
+	{
+		if (!left)
+		{
+			tmp = right;
+			right = right->next;
+		}
+		else 
+		{
+			if (!right)
+			{
+				tmp = left;
+				left = left->next;
+			}
+			else
+				ft_descendingorder_mergesort(&tmp, &left, &right);
+		}
+		if (!head)
+			head = tmp;
+		else
+			tail->next = tmp;
+		tail = tmp;
+	}
+	return (head);
+}
+
+t_merge	*ft_split_mergesort(t_merge *merge)
 {
 	t_merge	*left;
 	t_merge	*right;
-	t_merge	*tmp;
+	t_merge	*tmp; //
 	int		i;
+	int		nb_list;
 
+	if (merge->next == NULL)
+		return (merge);
 	i = 0;
-	right = *merge;
-	while (i < (size_lst / 2) - 1)
+	nb_list = ft_lstcount(&merge);
+	left = merge;
+	while (i < (nb_list / 2) - 1)
 	{
-		right = right->next;
+		left = left->next;
 		i++;
 	}
-	left = right->next;
-	right->next = NULL;
-	right = *merge;
+	right = left->next;
+	left->next = NULL;
+	left = merge;
 
+//AFFICHAGE/
 ft_putendl(GREEN"RIGHT"RESET);
 
 	tmp = right;
@@ -59,24 +140,15 @@ ft_putendl(GREEN"LEFT"RESET);
 		tmp = tmp->next;
 	}
 	ft_putendl("");
+//END - AFFICHAGE/
 
+	right = ft_split_mergesort(right);
+	left = ft_split_mergesort(left);
+	ft_putendl("JE SORS");
+	return ((ft_glue_mergesort(left, right)));
 }
 
 
-int		ft_count_lst(t_merge **merge)
-{
-	t_merge	*tmp;
-	int		nb_list;
-
-	nb_list = 0;
-	tmp = *merge;
-	while (tmp)
-	{
-		nb_list++;
-		tmp = tmp->next;
-	}
-	return (nb_list);
-}
 
 
 
@@ -93,7 +165,8 @@ int		main(int argc, char **argv)
 
 	if (!(merge = (t_merge *)malloc(sizeof(*merge))))
 		ft_exit(RED"error malloc t_merge *merge"RESET);
-	merge->i = ft_atoi(argv[1]);
+//	merge->i = ft_atoi(argv[1]);
+	merge->str = ft_strdup(argv[1]);
 	merge->next = NULL;
 
 	i = 2;
@@ -101,7 +174,8 @@ int		main(int argc, char **argv)
 	{
 		if (!(new = (t_merge *)malloc(sizeof(*new))))
 			ft_exit(RED"error malloc t_merge *merge"RESET);
-		new->i = ft_atoi(argv[i]);
+//		new->i = ft_atoi(argv[i]);
+		new->str = ft_strdup(argv[i]);
 		new->next = NULL;
 
 		tmp = merge;
@@ -112,7 +186,7 @@ int		main(int argc, char **argv)
 		i++;
 	}
 
-	i = ft_count_lst(&merge);
+	i = ft_lstcount(&merge);
 	ft_putstr("Nombre de liste : ");
 	ft_putnbr(i);
 	ft_putendl("");
@@ -129,7 +203,19 @@ ft_putendl(GREEN"AVANT"RESET);
 	}
 	ft_putendl("");
 
-	ft_split_mergesort(&merge, i);
+	merge = ft_split_mergesort(merge);
+	
+ft_putendl(GREEN"APRES"RESET);
+
+	tmp = merge;
+	while (tmp != NULL)
+	{
+		ft_putnbr(tmp->i);
+		if (tmp->next != NULL)
+			ft_putstr("\t");
+		tmp = tmp->next;
+	}
+	ft_putendl("");
 
 	return (0);
 }
